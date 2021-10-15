02e8fd03686b79b30b5c57c5510d313db0ead058
JIMENEZ417 <92607455+JIMENEZ417@users.noreply.github.com>
2021-10-15 16:11:46 -0500

    Add files via upload

---
+++ collage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Para_liz</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap');
+    </style>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Acme&family=Permanent+Marker&display=swap');
+    </style>
+
+
+
+</head>
+<body>
+    <header class="bg_animate">
+        <div class="header_nav">
+            <div class="contenedor">
+                <h1>Hola MAILOF</h1>
+                <nav>
+                    <a href="index.php">Bienvenida</a>
+                    <a href="collage.php">Collage</a>
+                    <a href="#">mirar xd</a>
+                </nav>
+            </div>
+        </div>
+        <audio src="aud/song.mp3" autoplay type="audio/mpeg"></audio>
+            
+        <div class="contenedor_collage">
+            <ul>
+                <li><img src="img/img1.JPG"></li>
+                <li><img src="img/img2.JPG"></li>
+                <li><img src="img/img3.JPG"></li>
+                <li><img src="img/img4.JPG"></li>
+            </ul>
+        </div>
+
+
+        </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Para_liz</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap');
+    </style>
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Acme&family=Permanent+Marker&display=swap');
+    </style>
+
+
+
+</head>
+<body>
+    <header class="bg_animate">
+        <div class="header_nav">
+            <div class="contenedor">
+                <h1>😍HOLA PEQUEÑA MORITA😍</h1>
+                <nav>
+                    <a href="index.php">Bienvenida</a>
+                    <a href="collage.php">Collage</a>
+                </nav>
+            </div>
+        </div>
+
+        <section class="banner contenedor">
+            <secrion class="banner_title">
+                <h2>
+                    Tal vez no sea el mejor sitio web pero, lo hice con mucha dedicacion y amor para ti. <br>
+                    TE AMO MAILOF ❤😍<br>
+                    ¡Conteplad la perfeccion mismisima!
+                </h2>
+                <a href="https://www.facebook.com/profile.php?id=100010054154394&sk=photos_by" class="llamanos">¡Clickea Aqui amor!</a>
+            </secrion>
+            <div class="banner_img">
+                <img src="img/unicornio.png">
+            </div>
+        </section>
+
+        <div class="burbujas">
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+            <div class="burbuja"></div>
+        </div>
+
+    </header>
+</body>
+</html>
